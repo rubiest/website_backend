0d168ae9055b07910a9ca5b73691670d003adb7a
AmirolAhmad <amirol@lizard-apps.com>
2017-10-03 19:15:03 +0800

    add product in_stock

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def check_stock(obj)
+    case obj.in_stock
+    when true
+      "<span class='badge badge-primary'>In Stock</span>".html_safe
+    when false
+      "<span class='badge badge-danger'>Out of Stock</span>".html_safe
+    else
+      "<span class='badge badge-danger'>#{obj.in_stock}</span>".html_safe
+    end
+  end
 end
