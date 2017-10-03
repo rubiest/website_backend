07692c66eefab58fe681ca6c5f28c5c0b0f1527e
AmirolAhmad <amirol@lizard-apps.com>
2017-10-03 14:49:49 +0800

    add devise for user auth

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'jquery-rails'
+gem 'devise'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks'
+  }
+
   root 'pages#index'
+
+  devise_scope :user do
+    get '/login' => 'users/sessions#new'
+    get '/register' => 'users/registrations#new'
+    get '/settings' => 'users/registrations#edit'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
